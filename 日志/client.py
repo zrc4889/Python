e5997b23738751e2311b5cf3e5bb3ce59b7e1a12
zrc4889@163.com <zrc4889@163.com>
2022-01-16 17:08:46 +0800

    2022年1月16日

--- 日志/client.py
+++ 日志/client.py
@@ -1,25 +1,18 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
 import socket
-
-ip_port = ('127.0.0.1', 9999)
-
-s = socket.socket()     # 创建套接字
-
-s.connect(ip_port)      # 连接服务器
-
-while True:     # 通过一个死循环不断接收用户输入，并发送给服务器
-    inp = input("请输入要发送的信息： ").strip()
-    if not inp:     # 防止输入空信息，导致异常退出
-        continue
-    s.sendall(inp.encode())
-
-    if inp == "exit":   # 如果输入的是‘exit’，表示断开连接
-        print("结束通信！")
+import sys
+address = ('127.0.0.1', 5005)  # 服务端地址和端口
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect(address)  # 尝试连接服务端
+except Exception:
+    print('[!] Server not found ot not open')
+    sys.exit()
+while True:
+    trigger = input('Input: ')
+    s.sendall(trigger.encode())
+    # data = s.recv(1024)
+    # data = data.decode()
+    # print('[Recieved]', data)
+    if trigger == '###':  # 自定义结束字符串
         break
-
-    server_reply = s.recv(1024).decode()
-    print(server_reply)
-
-s.close()       # 关闭连接
+s.close()
