5409582e5f96e3152fdc8a133386571cfd742b9e
zrc4889@163.com <zrc4889@163.com>
2022-02-04 14:34:34 +0800

    将聊天服务器添加了屏蔽词模块

--- 多线程服务器/client.py
+++ 多线程服务器/client.py
@@ -3,7 +3,8 @@ import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     host = input("host:")
     s.connect((host,1234)) # 仅可使用1234端口
-    msg = input("Send:")
-    s.sendall(msg.encode('utf-8'))
-    data = s.recv(1024)
-    print("Received:",data.decode('utf-8'))
+    while True:
+        msg = input("Send:")
+        s.sendall(msg.encode('utf-8'))
+        data = s.recv(1024)
+        print("Received:",data.decode('utf-8'))
--- 多线程服务器/server.py
+++ 多线程服务器/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+
 def handle_client(c, addr):
     print(addr, "connected.")
 
@@ -8,8 +9,14 @@ def handle_client(c, addr):
         data = c.recv(1024)
         if not data:
             break
-        print((addr)," says:",data.decode('utf-8'))
-        c.sendall(data) # 重复发送消息
+        f1 = ["迷你", "mini", "MD", "cmn", "TMD", "他妈的", "马牛逼", "丁三石", "吊", "蛋蛋", "鸡鸡", "草", "智障", "NB", "牛逼", "大便", "屎", "尿", "你妈", "囸", "SB", "VIP", "vip", "MMP", "mmp", "屁", "仙人", "先人", "妈逼", "fuck", "FUCK", "Fuck",
+              "王八蛋", "你奶奶的", "你妈死了", "逼", "傻逼", "煞笔", "沙比", "沙壁", "nmsl", "sb", "马化腾", "麻花疼", "丁三石", "作弊", "脑子有病", "操", "卧槽", "我操", "握草", "特么的", "你妈的", "妈蛋", "装逼", "nm", "jb", "操", "美国", "中国", "傻", "猪", "变态", "死", "歹"]
+        speak = data.decode('utf-8')
+        for i in f1:
+            speak = speak.replace(i, '*')
+        print(speak)
+        print((addr), " says:", speak)
+        c.sendall(speak.encode('utf-8'))  # 重复发送消息
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("192.168.6.247", 1234))
